fa92b4f00fdd38265193649350b31e32e0bb85df
unknown <you@example.com>
2020-12-30 01:19:27 +0900

    헤더 퍼블리싱 완성

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './Components/Header';
 function App() {
 	return (
 		<div className="App">
-			<h1>Let's build the TINDER Clone App </h1>
-
 			{/* Header */}
 			<Header />
 			{/* Tinder Cards */}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
 	return (
-		<div>
-			<h1>I'm a Header</h1>
+		<div className="header">
+			<IconButton>
+				<PersonIcon className="header__icon" fontSize="large" />
+			</IconButton>
+
+			<img
+				className="header__logo"
+				src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+				alt="tinder_logo"
+			/>
+
+			<IconButton>
+				<ForumIcon className="header__icon" fontSize="large" />
+			</IconButton>
 		</div>
 	);
 }
