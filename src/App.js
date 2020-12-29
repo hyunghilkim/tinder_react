fbbd99538949f5c3f7a4a848c807ea51fc78fcfa
unknown <you@example.com>
2020-12-30 00:31:27 +0900

    tinder start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Let's build the TINDER Clone App </h1>
+
+			{/* Header */}
+			<Header />
+			{/* Tinder Cards */}
+			{/*  Buttons below tinder Cards */}
+
+			{/* Chats screen */}
+			{/* Individual chat screen */}
+		</div>
+	);
 }
 
 export default App;
