9a1db611d04d426ef242b8cb9651d7fab0cf62d1
unknown <you@example.com>
2021-01-14 06:45:30 +0900

    버튼 퍼블리싱 완성

--- src/Components/SwipeButtons.js
+++ src/Components/SwipeButtons.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReplayIcon from '@material-ui/icons/Replay';
 import CloseIcon from '@material-ui/icons/Close';
-import StarRateIcon from '@material-ui/icons/StarRate';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
 import './SwipteButtons.css';
 
 function SwipeButtons() {
 	return (
 		<div className="swipeButtons">
-			<ReplayIcon fontSize="large" />
-			<CloseIcon fontSize="large" />
-			<StarRateIcon fontSize="large" />
-			<FavoriteIcon fontSize="large" />
-			<FlashOnIcon fontSize="large" />
+			<IconButton className="swipeButtons__repeat">
+				<ReplayIcon fontSize="large" />
+			</IconButton>
+			<IconButton className="swipeButtons__left">
+				<CloseIcon fontSize="large" />
+			</IconButton>
+			<IconButton className="swipeButtons__right">
+				<FavoriteIcon fontSize="large" />
+			</IconButton>
 		</div>
 	);
 }
